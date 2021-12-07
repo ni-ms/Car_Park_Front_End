ad9167defac74e05b12f44400bf76b0cf7f243cd
nikhilmsharma <77634325+nikhilmsharma@users.noreply.github.com>
2021-12-07 14:31:53 +0530

    Admin

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        username: "",
+        admin: "",
+
+    },
+    mutations: {
+        adduser(state, cuser) {
+            state.username = cuser;
+        },
+        addadmin(state, cadmin) {
+            state.admin = cadmin;
+
+        }
+
+
+    },
+    actions: {},
+    modules: {}
 })
